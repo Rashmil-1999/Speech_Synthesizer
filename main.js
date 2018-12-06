bf3a672387d9387580657aa568bd2dc4f4eb5516
Siddharth Sanghavi <sanghavisidd@gmail.com>
2018-12-07 02:50:40 +0530

    First commit

---
+++ main.js
@@ -0,0 +1,37 @@
+const msg = new SpeechSynthesisUtterance();
+let voices = [];
+const voicesDropdown = document.querySelector('[name="voice"]');
+const options = document.querySelectorAll('[type="range"], [name="text"]');
+const speakButton = document.querySelector('#speak');
+const stopButton = document.querySelector('#stop');
+msg.text = document.querySelector('[name="text"]').value;
+
+function populateVoices() {
+  voices = this.getVoices();
+  voicesDropdown.innerHTML = voices.filter(voice => voice.name !== "Microsoft David Desktop - English (United States)" && voice.name !== "Microsoft Zira Desktop - English (United States)")
+                                   .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
+                                   .join("");
+}
+
+function setVoice() {
+  msg.voice = voices.find(voice => voice.name === this.value);
+  toggle();
+}
+
+function setOption() {
+  msg[this.name] = this.value;
+  toggle();
+}
+
+function toggle(startOver = true) {
+  speechSynthesis.cancel();
+  if (startOver) {
+    speechSynthesis.speak(msg);
+  }
+}
+
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
+voicesDropdown.addEventListener('change', setVoice);
+options.forEach(option => option.addEventListener('change', setOption))
+speakButton.addEventListener('click', toggle);
+stopButton.addEventListener('click', function() {toggle(false);})
